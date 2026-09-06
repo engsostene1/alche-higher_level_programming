d22a2aa617b28a72617410f0f50180be31fbb75b
engsostene1 <s.uwihangan@alustudent.com>
2026-09-06 11:36:21 -0700

    Fix 0-square.py: add module and class documentation, ensure full ALX compliance

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
+"""
+Module that defines an empty Square class.
+"""
+
+
 class Square:
-    """An empty Square class."""
+    """
+    Empty class that defines a square.
+    """
     pass
